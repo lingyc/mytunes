a0ae84059fc7fb7229fda2ecd2d821698cca5206
lingyc <lingchen215@gmail.com>
2016-06-22 19:14:18 -0700

    Complete all tests for SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,17 +4,24 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-    console.log(this);
+    // console.log(this);
     this.on('add', function() {
       if (this.models.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function() {
+      this.shift();
+      if (this.length > 0) {
         this.playFirst();
       }
     }, this);
   },
 
   playFirst: function() {
-    var song = this.models[0];
-    this.models.shift();
+    var song = this.at(0);
+    song.play();
   }
 
 });
